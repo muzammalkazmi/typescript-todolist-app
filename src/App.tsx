e87b3e62af3fea2f682845e4b1f5c0c9ec9459a4
muzammal kazmi <muzammalkazmi@gmail.com>
2021-10-18 17:18:10 +0500

    added function to toggle todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {TodoListItem} from './TodoListItem'
 
 
 function App() {
-  const todos:Todo[]=[
+  const initialtodos:Todo[]=[
     {text:'Reconcile the invoices with tppl',completed:true},
     {text:'Reconcile the bank statements',completed:false}
   ]
+  const [todos,settodos]=useState(initialtodos)
+  console.log(todos)
+  const toggleTodo=(selectedTodo:Todo)=>{
+    const newTodo=todos.map((todo)=>{
+      if(todo===selectedTodo){
+        return{
+          ...todo,
+       completed:!todo.completed
+
+        }
+      }
+    })
+    settodos(newTodo)
+  }
   return (
     <div className="App">
       <h1>React ToDo List App with Typescript</h1>
